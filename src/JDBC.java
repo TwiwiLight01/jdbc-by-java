35e52833c5171643267c80f6d64d0766a89633c2
TwiwiLight01 <alouizmahmoudia@gmail.com>
2025-10-18 17:19:28 +0100

    last step

---
+++ src/JDBC.java
@@ -0,0 +1,35 @@
+import java.sql.*;
+
+public class JDBC {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Connection connection = DriverManager.getConnection(
+
+                    "jdbc:mysql://127.0.0.1:3306/login_schema",
+                    "root",
+                    "password"
+            );
+
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
+
+            while(resultSet.next()) {
+
+                System.out.println(resultSet.getString("username"));
+                System.out.println(resultSet.getString("password"));
+
+            }
+
+
+        } catch(SQLException e) {
+
+            e.printStackTrace();
+        }
+
+    }
+
+}
